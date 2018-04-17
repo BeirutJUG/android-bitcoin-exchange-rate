6dd0a4df2b78096bb1090e8dc77a7beb17863458
Ahmad Shami <ashami@murex.com>
2018-04-17 16:47:43 +0300

    Add refresh exchange rate button

--- app/src/main/java/com/murex/bitcoinrate/MainActivity.java
+++ app/src/main/java/com/murex/bitcoinrate/MainActivity.java
@@ -1,13 +1,49 @@
 package com.murex.bitcoinrate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.murex.bitcoinrate.network.BitcoinRate;
+import com.murex.bitcoinrate.network.NetworkClient;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLatestBitcoinRates();
+            }
+        });
+    }
+
+    /**
+     * Send an api call to fetch the latest bitcoin rates
+     */
+    private void getLatestBitcoinRates() {
+        NetworkClient.getLatestRates().enqueue(new Callback<Map<String, BitcoinRate>>() {
+            @Override
+            public void onResponse(Call<Map<String, BitcoinRate>> call, Response<Map<String, BitcoinRate>> response) {
+                Log.d(TAG, "Successful!");
+            }
+
+            @Override
+            public void onFailure(Call<Map<String, BitcoinRate>> call, Throwable t) {
+                Log.d(TAG, "Failed", t);
+            }
+        });
     }
 }
